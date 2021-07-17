7d5e4118c365e63ac61027c96392bc25ee608f7a
pizzaminded <pizzaminded@users.noreply.github.com>
2021-07-17 21:38:21 +0200

    refactor: Move UserInterface.php to contracts

--- src/Jadob/Contracts/Core/UserInterface.php
+++ src/Jadob/Contracts/Core/UserInterface.php
@@ -7,4 +7,19 @@ namespace Jadob\Contracts\Core;
 interface UserInterface
 {
 
+    /**
+     * @return string[]
+     */
+    public function getRoles();
+
+    /**
+     * @return string
+     */
+    public function getPassword();
+
+    /**
+     * @return mixed
+     */
+    public function getUsername();
+
 }
